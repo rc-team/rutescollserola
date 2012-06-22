0a51d650ae088e4b0cbffa7f4a05325294ef67a1
Pau Haro Negre <pauharo@gmail.com>
2012-06-22 13:08:33 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rutescollserola::Application.config.secret_token = 'eb258690d3adaa0ec8b0eae0168a606f00b4de7e00b681e6023911ec6ff1622a21188024955196ee8ffde870a532677d92c0ffbed22be23cbee58fb7bef88e23'

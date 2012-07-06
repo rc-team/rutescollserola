c0e9c253f4845d2c29c1189d88c5569f92b5526b
Pau Haro Negre <pauharo@gmail.com>
2012-07-05 20:16:46 -0400

    Use RSpec shared example to eliminate test duplication

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,32 +6,41 @@ describe "StaticPages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h2', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h2', text: 'Collserola en bicicleta') }
-    it { should have_selector('title', text: full_title('')) }
+    let(:heading) { 'Collserola en bicicleta' }
+    let(:page_title) { '' }
+    it_should_behave_like "all static pages"
   end
 
   describe "Who page" do
     before { visit who_path }
 
-    it { should have_selector('h2', text: 'Qui l\'ha fet') }
-    it { should have_selector('title', text: full_title('Qui hi ha al darrera')) }
+    let(:heading) { 'Qui l\'ha fet' }
+    let(:page_title) { 'Qui hi ha al darrera' }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h2', text: 'Objectius del web') }
-    it { should have_selector('title', text: full_title('Sobre el web')) }
+    let(:heading) { 'Objectius del web' }
+    let(:page_title) { 'Sobre el web' }
+    it_should_behave_like "all static pages"
   end
 
   describe "Downloads page" do
     before { visit downloads_path }
 
-    it { should have_selector('h2', text: 'Sel·lecció de fotografies') }
-    it { should have_selector('title', text: full_title('Descàrregues')) }
+    let(:heading) { 'Sel·lecció de fotografies' }
+    let(:page_title) { 'Descàrregues' }
+    it_should_behave_like "all static pages"
   end
 end
 

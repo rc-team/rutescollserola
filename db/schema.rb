d31f013aa24ead4ad4203aeca9242266ef5f2ecb
Pau Haro Negre <pauharo@gmail.com>
2013-01-11 10:21:23 -0700

    Bundle update rails

--- db/schema.rb
+++ db/schema.rb
@@ -50,9 +50,11 @@ ActiveRecord::Schema.define(:version => 20121209185121) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
   create_table "vertices", :force => true do |t|
     t.string   "name"

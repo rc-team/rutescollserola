b02aa37c2ae8ce9962a7a4ee000a05d8ca938c35
Pau Haro Negre <pauharo@gmail.com>
2013-08-11 17:33:46 -0600

    Refactor the new and edit User views to use a partial

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -89,10 +89,10 @@ describe "User Pages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Name",             with: "Example User"
+        fill_in "Email",            with: "user@example.com"
+        fill_in "Password",         with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
